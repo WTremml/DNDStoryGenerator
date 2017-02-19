0b9ab594e60788702993e7ddbf8bfe8d6edbc0a6
emilyjchao <emily.chao@ymail.com>
2017-02-19 14:01:14 -0500

    Create monster.h

---
+++ monster.h
@@ -0,0 +1,27 @@
+/*
+ * monster.h
+ *
+ *  Created on: Feb 11, 2017
+ *      header file for monster class
+ *      	extends character class
+ */
+
+#ifndef MONSTER_H
+#define MONSTER_H
+
+#include "character.h"
+
+class Monster: public Character {
+private:
+	int aggressive;			//how aggressive monster is
+	int wandering;			//whether monster moves or not
+	int type;				//0 if small, 1 if big monster
+public:
+	Monster();				//default constructor
+	Monster(int type, int wander, int agg, int x, int y, int z);
+	Monster(Monster& old);	//copy constructor
+	~Monster() {}			//destructor
+	int getType();			//get type of monster
+};
+
+#endif
